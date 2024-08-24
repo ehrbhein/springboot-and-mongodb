2364934712289550587b5fd96a0893208e80e7f9
ehrbhein <theofficial.irvingil@gmail.com>
2024-08-24 15:06:55 +0800

    :sparkles: feat: add model classes

---
+++ src/main/java/com/sideproject/springboot_and_mongodb/student/Address.java
@@ -0,0 +1,11 @@
+package com.sideproject.springboot_and_mongodb.student;
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+  private String country;
+  private String city;
+  private String postCode;
+}
---
+++ src/main/java/com/sideproject/springboot_and_mongodb/student/Gender.java
@@ -0,0 +1,5 @@
+package com.sideproject.springboot_and_mongodb.student;
+
+public enum Gender {
+  MALE, FEMALE
+}
