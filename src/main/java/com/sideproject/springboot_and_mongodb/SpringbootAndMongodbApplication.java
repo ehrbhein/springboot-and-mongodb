b8439eeb0bf9b33ba1034575f35e01f152b2ae0b
ehrbhein <theofficial.irvingil@gmail.com>
2024-08-24 15:39:27 +0800

    :sparkles: feat: add search and validation
    for inserting test data on runtime.

--- src/main/java/com/sideproject/springboot_and_mongodb/SpringbootAndMongodbApplication.java
+++ src/main/java/com/sideproject/springboot_and_mongodb/SpringbootAndMongodbApplication.java
@@ -11,6 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 @SpringBootApplication
 public class SpringbootAndMongodbApplication {
@@ -20,13 +23,36 @@ public class SpringbootAndMongodbApplication {
   }
 
 
+  /*
+   * @info: Succeeding application runs will throw and error when  `auto-index-creation` is enabled
+   * */
   @Bean
-  CommandLineRunner runner(StudentRepository studentRepository) {
+  CommandLineRunner runner(StudentRepository studentRepository, MongoTemplate mongoTemplate) {
     return args -> {
       Address address = new Address("England", "London", "NE9");
       Student student = new Student("Jamila", "Ahmed", "jahmed@gmail.com", Gender.FEMALE, address, List.of("Computer Science"), BigDecimal.TEN, LocalDateTime.now());
 
-      studentRepository.insert(student);
+      /*
+       * @note: We implement a find criteria to search for existing students on the database.
+       * It should only insert new student if no other students are found with the same email.
+       */
+      Criteria criteria = Criteria.where("email").is(student.getEmail());
+
+      Query query = new Query();
+      query.addCriteria(criteria);
+
+      List<Student> students = mongoTemplate.find(query, Student.class);
+
+      if (students.size() > 1) {
+        throw new IllegalStateException("Found more than one student with email: " + student.getEmail());
+
+      } else if (!students.isEmpty()) {
+        System.out.println(student + " already exists.");
+
+      } else {
+        studentRepository.insert(student);
+
+      }
     };
   }
 }
--- src/main/java/com/sideproject/springboot_and_mongodb/domain/student/Student.java
+++ src/main/java/com/sideproject/springboot_and_mongodb/domain/student/Student.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -17,6 +18,7 @@ public class Student {
   private String id;
   private String firstName;
   private String lastName;
+  @Indexed(unique = true)
   private String email;
   private Gender gender;
   private Address address;
