d02b7d5e4ed4d591bae8d9e5e4b1b4db8a51d88c
ehrbhein <theofficial.irvingil@gmail.com>
2024-08-24 13:02:40 +0800

    initial commit

---
+++ src/main/java/com/sideproject/springboot_and_mongodb/SpringbootAndMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.springboot_and_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAndMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/springboot_and_mongodb/SpringbootAndMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.springboot_and_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAndMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
