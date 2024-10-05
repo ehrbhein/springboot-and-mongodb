a92bbfb01801d65de4efef2b4071ae007669f7bb
ehrbhein <theofficial.irvingil@gmail.com>
2024-10-05 13:12:41 +0800

    :recycle: refactor: use allArgsConstructor
    instead of autowired

--- src/main/java/com/sideproject/springboot_and_mongodb/domain/student/StudentService.java
+++ src/main/java/com/sideproject/springboot_and_mongodb/domain/student/StudentService.java
@@ -1,18 +1,14 @@
 package com.sideproject.springboot_and_mongodb.domain.student;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class StudentService {
 
   private final StudentRepository studentRepository;
-
-  @Autowired
-  public StudentService(StudentRepository studentRepository) {
-    this.studentRepository = studentRepository;
-  }
 
   public List<Student> getAllStudents() {
     return studentRepository.findAll();
